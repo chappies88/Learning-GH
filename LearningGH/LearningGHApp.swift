f8c9cccb1cef69fb1d54c30d98761b94c97ce93d
chappies <darith.isd@gmail.com>
2022-04-09 23:01:06 +0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by chappie on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
